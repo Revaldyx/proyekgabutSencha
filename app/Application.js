717d033d396c3256b187bda4f4d8882b408b095b
Reza Fahlevi <rezafahleviex@gmail.com>
2020-11-03 00:35:43 +0700

    buat login

--- app/Application.js
+++ app/Application.js
@@ -1,8 +1,3 @@
-/**
- * The main application class. An instance of this class is created by app.js when it
- * calls Ext.application(). This is the ideal place to handle application launch and
- * initialization details.
- */
 Ext.define('proyekgabutSencha.Application', {
     extend: 'Ext.app.Application',
 
@@ -20,7 +15,29 @@ Ext.define('proyekgabutSencha.Application', {
     ],
 
     launch: function () {
-        // TODO - Launch the application
+        var loggedIn;
+        loggedIn = localStorage.getItem("loggedIn"); //ambil session
+        
+        if (!loggedIn) {
+            this.overlay = Ext.Viewport.add({ //menambahan viewport
+                xtype: 'login',
+                floated: true,
+                showAnimation: {
+                    type: 'popIn',
+                    duration: 250,
+                    easing: 'ease-out'
+                },
+                hideAnimation: {
+                    type: 'popOut',
+                    duration: 250,
+                    easing: 'ease-out'
+                },
+                width: "100%",
+                height: "100%",
+                scrollable: true
+            });
+        }
+        this.overlay.show();
     },
 
     onAppUpdate: function () {
---
+++ app/view/login/loginController.js
@@ -0,0 +1,90 @@
+Ext.define('proyekgabutSencha.view.form.loginController', {
+    extend: 'Ext.app.ViewController',
+    alias: 'controller.login',
+
+    onLogin: function () {
+        var form = this.getView();
+        var me = this;
+
+        var name = form.getFields('name').getValue();
+        var password = form.getFields('password').getValue();
+        if (name == 'admin' && password == 'admin') {
+            Ext.Msg.alert('Login Success', 'You have been logged in');
+            form.hide();
+        }
+        else {
+            Ext.Msg.alert('Konfirmasi', 'Username/Password Anda Salah', this);
+        }
+    },
+    // onRegis: function() {
+        //             if (!this.overlay) {
+        //                 this.overlay = Ext.Viewport.add({
+        //                     xtype: 'panel',
+        //                     floated: true,
+        //                     modal: true,
+        //                     hideOnMaskTap: true,
+        //                     showAnimation: {
+        //                         type: 'popIn',
+        //                         duration: 250,
+        //                         easing: 'ease-out'
+        //                     },
+        //                     hideAnimation: {
+        //                         type: 'popOut',
+        //                         duration: 250,
+        //                         easing: 'ease-out'
+        //                     },
+        //                     centered: true,
+        //                     width :"80%",
+        //         			height :"80%",
+                            
+        //                         title: 'Pendaftaran',
+        //                         id:'regis',
+        //                     	items:[
+        //                     		{
+        //                     			label:'Nama',
+        //                     			xtype:'textfield',
+        //                     			name:'username',
+        //                     		},
+        //                     		{
+        //                     			label:'password',
+        //                     			xtype:'passwordfield',
+        //                     			name:'password',
+        //                     		},
+        //                     		{
+        //                     			label:'Alamat',
+        //                     			xtype:'textfield',
+        //                     			name:'alamat',
+        //                     		},
+        //                     		{
+        //                     			label:'Pekerjaan',
+        //                     			xtype:'textfield',
+        //                     			name:'pekerjaan',
+        //                     		},
+        //                     		{
+        //                     			xtype:'button',
+        //                 				text: 'Daftar',
+        //                 				ui: 'action',
+        //                 				handler: function(){
+        //                 					Ext.Msg.alert('Register Success','You have been Registered');
+        //                 				}
+        //             				},
+        //             				{
+        //                                 xtype:'button',
+        //                                 text: 'Kembali',
+        //                                 style: 'margin: 1em 0 1em 1em',
+        //                                 style:'background-color:#E61607;margin:14px;color: white',
+        //                                 ui: 'action',
+        //                                 handler: function(){
+        //                                     var reg = Ext.getCmp('regis');
+        //                                     reg.close();
+        //                                   }
+        //             				}
+        //                     	],
+    
+    
+        //                     scrollable: true
+        //                 });
+        //             }this.overlay.show();
+        // },
+   
+});
